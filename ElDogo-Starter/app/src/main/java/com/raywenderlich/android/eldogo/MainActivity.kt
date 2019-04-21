8af0d234b371b6de524c9bf1e10e2a59ad3b0298
aeonfluffy <shadow_fox9503@yahoo.com>
2019-04-21 02:41:02 -0500

    made fragments display

--- ElDogo-Starter/app/src/main/java/com/raywenderlich/android/eldogo/MainActivity.kt
+++ ElDogo-Starter/app/src/main/java/com/raywenderlich/android/eldogo/MainActivity.kt
@@ -37,6 +37,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.root_layout, DogListFragment.newInstance(), "dogList")
+                .commit()
+        }
     }
 
 }
